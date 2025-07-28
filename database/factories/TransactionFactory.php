a4fbed7b500c388c2f677a026146a82d227fb991
Lucas Heber <lucas.heber07@gmail.com>
2025-07-28 16:57:06 -0300

    feat: add TransactionFactory and TransactionSeeder for generating transaction data

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'document' => $this->faker->randomElement([$this->cpf(), $this->cnpj()]),
+            'amount'   => $this->faker->randomFloat(2, 100, 10000), // Amount in dollars
+            'currency' => $this->faker->randomElement(['USD', 'EUR', 'BRL']),
+            'status'   => \App\Enums\TransactionStatus::PENDING->value,
+        ];
+    }
+
+    private function cpf(): string
+    {
+        // Generate a random CPF number
+        return $this->faker->numerify('###.###.###-##');
+    }
+
+    private function cnpj(): string
+    {
+        // Generate a random CNPJ number
+        return $this->faker->numerify('##.###.###/####-##');
+    }
+}
