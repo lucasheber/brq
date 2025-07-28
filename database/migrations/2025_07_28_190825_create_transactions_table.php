a47191e48fcde4b93b5eedd053c806ebe1a73d14
Lucas Heber <lucas.heber07@gmail.com>
2025-07-28 16:56:51 -0300

    feat: create migration for transactions table with necessary fields and status enum

---
+++ database/migrations/2025_07_28_190825_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+use App\Enums\TransactionStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            // Uncomment the following line if you have a user_id foreign key
+            // $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('document');
+            $table->integer('amount')->unsigned();
+            $table->string('currency', 3);
+            $table->enum('status', array_column(TransactionStatus::cases(), 'value'))
+                ->default(TransactionStatus::PENDING->value)
+                ->comment('Transaction status');
+            $table->string('motivation')->nullable()->comment('Reason for transaction status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
