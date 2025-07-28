891dc789b73dfd80854373774cb684e71f922719
Lucas Heber <lucas.heber07@gmail.com>
2025-07-28 16:47:07 -0300

    test: implement model configuration in AppServiceProvider for strict mode and unguarding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configModels();
+    }
+
+    private function configModels(): void
+    {
+        // Disable the fillable property for all models
+        Model::unguard();
+
+        // Make strict mode the default for all models
+        Model::shouldBeStrict();
     }
 }
