a3de3814f4a7e9d39d9c4f11fdf066ab02489b25
Lucas Heber <lucas.heber07@gmail.com>
2025-07-29 21:29:35 -0300

    feat: add description field to Transaction model and corresponding migration

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -23,6 +23,7 @@ class TransactionFactory extends Factory
             'user_id'    => $this->faker->randomElement(User::pluck('id')->toArray()),
             'document'   => $this->faker->randomElement([$this->cpf(), $this->cnpj()]),
             'amount'     => $this->faker->randomFloat(2, 100, 10000), // Amount in dollars
+            'description' => $this->faker->sentence, // New description field
             'currency'   => $this->faker->randomElement(['USD', 'EUR', 'BRL']),
             'status'     => $this->faker->randomElement(\App\Enums\TransactionStatus::cases())->value,
             'location'   => $this->faker->city, // Assuming location is a city name
